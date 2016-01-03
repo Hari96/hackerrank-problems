8a1a43488fbf7694f52badab279e21850c222cdb
Hari96 <bob@rgbassociates.co.uk>
2016-01-03 14:24:37 +0000

    Amended sherlock-beast file

--- sherlock-beast.js
+++ sherlock-beast.js
@@ -1,3 +1,19 @@
+//not sure if using the two output functions is more efficient really
+
+function output5(count, output) {
+  for (var j = 1; j <= count; j++) {
+    output += "555";
+  }
+  return output;
+}
+
+function output3(count, output) {
+  for (var j = 1; j <= count; j++) {
+    output += "33333";
+  }
+  return output;
+}
+
 function processData(input) {
   var a = input.split("\n");//takes account of way data is input on hackerrank
   var T = parseInt(a[0]);
@@ -7,17 +23,12 @@ function processData(input) {
     if (n < 3 || n === 4 || n === 7) {
       console.log(-1);
     }
-    else if (n === 3) {
-      console.log(555);
-    }
     else if (n === 5) {
       console.log(33333);
     }
     else if (n%3 === 0) {
         c2 = n/3;
-        for (var m = 1; m <= c2; m++) {
-          output += "555";
-        }
+        output = output5(c2, output);
         console.log(output);
         output = "";
         c2 = 0;
@@ -27,20 +38,14 @@ function processData(input) {
         n -= 5;
         c1++;
         if (n === 5) {
-          for (var j = 1; j <= c1; j++) {
-            output += "33333";
-          }
+          output = output3(c1, output);
           console.log(output);
           output = "";
         }
         else if (n%3 === 0) {
           c2 = n/3;
-          for (var k = 1; k <= c2; k++) {
-            output += "555";
-          }
-          for (var l = 1; l <= c1 - 1; l++) {
-            output += "33333";
-          }
+          output = output5(c2, output);
+          output += output3(c1 - 1, output);
           console.log(output);
           output = "";
           n = 0;
