070566ebdf85adedd44d2accb324edec9c5db1b1
Hari96 <bob@rgbassociates.co.uk>
2016-01-04 23:26:53 +0000

    Amended sherlock-and-squares file

--- sherlock-and-square.js
+++ sherlock-and-square.js
@@ -3,10 +3,11 @@ function processData(input) {
   var end = parseInt(a[0]);
   for (var i = 1; i <= end; i++) {
       var nums = a[i].split(" ");
+      // following method is much, much better than checking each digit for a perfect square!!
       var s1 = Math.sqrt(parseInt(nums[0]));
       var s2 = Math.sqrt(parseInt(nums[1]));
       var count = Math.floor(s2) - Math.floor(s1);
-      if (s2 % 1 === 0) {
+      if (s1 % 1 === 0) {
         count++;
       }
       console.log(count);
